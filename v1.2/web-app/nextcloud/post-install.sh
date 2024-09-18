4fcf8f336a2f60cbf69d1055cf3fe19715ded68e
Alejandro Apestegui <pjesusovich@gmail.com>
2024-09-18 13:44:14 -0500

    kallpa v1.2

--- v1.2/web-app/nextcloud/post-install.sh
+++ v1.2/web-app/nextcloud/post-install.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 docker compose exec -u 33 nextcloud php occ app:install memories
+docker compose exec -u 33 nextcloud php occ app:install previewgenerator
+docker compose exec -u 33 nextcloud php occ app:install recognize
+docker compose exec -u 33 nextcloud php occ app:install facerecognition
+
+# docker compose exec -u 33 nextcloud php occ config:system:set filelocking.enabled --value=false
